f63946fa25673e35359b7d5b25ec709c02ed4ba5
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-14 15:01:34 +0100

    Well

--- blog/terraform/deploy/deployments.tf
+++ blog/terraform/deploy/deployments.tf
@@ -19,7 +19,7 @@ resource "kubernetes_deployment" "app" {
       }
       spec {
         container {
-          image = var.docker-image
+          image = "eu.gcr.io/crafty-clover-301509/unoterr1/blog_comp:latest"
           name  = var.app
           port {
             name = "port-3000"
@@ -41,6 +41,7 @@ resource "kubernetes_deployment" "app" {
             name  = "DATABASE_USERNAME"
             value = "blog_backend"
           }
+
         }
       }
     }
