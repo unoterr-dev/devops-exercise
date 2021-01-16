b71c6ac9ecb3b8042817877aa638da2499b2c472
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-16 23:07:03 +0100

    Excecute

--- blog/terraform/test/main.tf
+++ blog/terraform/test/main.tf
@@ -1,6 +1,6 @@
   resource "null_resource" "apply" {
     provisioner "local-exec" {
-        command = "cd /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/ && gcloud container clusters describe terraform-cluster --zone='europe-north1'"
+        command = "cd /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/ && ./gcloud container clusters describe terraform-cluster --zone='europe-north1'"
      //   interpreter = ["/bin/zsh", "-c"]
     } 
   }
