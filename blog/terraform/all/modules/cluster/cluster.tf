99331a3c654e1b1f907e5010908482351daa67d5
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-16 17:06:31 +0100

    I dunnno

--- blog/terraform/all/modules/cluster/cluster.tf
+++ blog/terraform/all/modules/cluster/cluster.tf
@@ -31,7 +31,7 @@ resource "google_container_node_pool" "primary_node" {
       "https://www.googleapis.com/auth/cloud-platform",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/service.management",
-      "https://www.googleapis.com/auth/devstorage.read_only",
+//      "https://www.googleapis.com/auth/devstorage.read_only",
     ]
   }
   // NOT VERY GOOD PRACTICCE
