8ec42db9f23798df898100baa7bd2a24592ee57a
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-16 23:02:18 +0100

    If true

--- blog/terraform/test/main.tf
+++ blog/terraform/test/main.tf
@@ -1,6 +1,6 @@
   resource "null_resource" "apply" {
     provisioner "local-exec" {
-        command = "gcloud container clusters describe terraform-cluster --zone='europe-north1'"
-        interpreter = ["/bin/zsh", "-c"]
+        command = "./usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/gcloud container clusters describe terraform-cluster --zone='europe-north1'"
+     //   interpreter = ["/bin/zsh", "-c"]
     } 
   }
