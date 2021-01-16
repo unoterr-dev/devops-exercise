fcbde388498cbf0a1e4c5d64e91c4fdf2f03a9c9
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-16 17:37:26 +0100

    Well

--- blog/terraform/all/modules/cluster/cluster.tf
+++ blog/terraform/all/modules/cluster/cluster.tf
@@ -43,12 +43,13 @@ resource "google_container_node_pool" "primary_node" {
   */
 }
 
-provider "kubernetes" {
+provider "kubernetes" { /*
   load_config_file = false
   host  = "https://${google_container_cluster.primary.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
     google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
-  )
+  ) */
 }
+
 
