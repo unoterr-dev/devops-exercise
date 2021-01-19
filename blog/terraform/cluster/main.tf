93be1dc3d932916ec95fbef1d36f7d260f99a3ab
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-19 11:45:58 +0100

    Spell

--- blog/terraform/cluster/main.tf
+++ blog/terraform/cluster/main.tf
@@ -13,8 +13,8 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = "terraform-net"
-  subnetwork = "terraform-net"
+  network    = "terraform-vpc"
+  subnetwork = "terraform-sub"
 
 }
 # gcloud container clusters get-credentials terraform-cluster --zone="europe-north1""
