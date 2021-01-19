8a61e9f92692526791dcf671edd29c673bbc8432
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-19 14:52:46 +0100

    Parse json

--- blog/terraform/all/deploy.sh
+++ blog/terraform/all/deploy.sh
@@ -1,7 +1,13 @@
+terraform output -json
+
+terraform output -json | jq -r '.pass.value'  
+
 IMAGE="c97151a"
-HOST="35.228.181.33"
-NAME=""
-USER=""
-PASS=""
-envsubst < blog.yaml > blog_tmp.yaml
-kubectl apply -f blog_tmp.yaml
+HOST=$(terraform output -json | jq -r '.host.value')
+NAME=$(terraform output -json | jq -r '.name.value')
+USER=$(terraform output -json | jq -r '.user.value')
+PASS=$(terraform output -json | jq -r '.pass.value')
+
+echo ${HOST}${NAME}${USER}${PASS}
+#envsubst < blog.yaml > blog_tmp.yaml
+#kubectl apply -f blog_tmp.yaml
--- blog/terraform/all/outputs.tf
+++ blog/terraform/all/outputs.tf
@@ -1,9 +1,11 @@
 output "user" {
   value = module.secrets.secret_user
+  sensitive = true
 }
 
 output "pass" {
   value = module.secrets.secret_pass
+  sensitive = true
 }
 
 output "host" {
