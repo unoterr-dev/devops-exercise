9497f9dadb2e97861bd81ff3755506b973aea4bc
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-19 15:03:58 +0100

    Export

--- blog/terraform/all/deploy.sh
+++ blog/terraform/all/deploy.sh
@@ -1,8 +1,8 @@
-IMAGE="c97151a"
-HOST=$(terraform output -json | jq -r '.host.value')
-NAME=$(terraform output -json | jq -r '.name.value')
-USER=$(terraform output -json | jq -r '.user.value')
-PASS=$(terraform output -json | jq -r '.pass.value')
+export IMAGE="c97151a"
+export HOST=$(terraform output -json | jq -r '.host.value')
+export NAME=$(terraform output -json | jq -r '.name.value')
+export USER=$(terraform output -json | jq -r '.user.value')
+export PASS=$(terraform output -json | jq -r '.pass.value')
 
 envsubst < blog.yaml > blog_tmp.yaml
 kubectl apply -f blog_tmp.yaml
