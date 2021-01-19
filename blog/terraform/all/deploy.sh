d52522baf96e7fcad175c13c7e42fe7dacb2d3f2
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-19 14:56:34 +0100

    Final..

--- blog/terraform/all/deploy.sh
+++ blog/terraform/all/deploy.sh
@@ -1,13 +1,10 @@
-terraform output -json
-
-terraform output -json | jq -r '.pass.value'  
-
 IMAGE="c97151a"
 HOST=$(terraform output -json | jq -r '.host.value')
 NAME=$(terraform output -json | jq -r '.name.value')
 USER=$(terraform output -json | jq -r '.user.value')
 PASS=$(terraform output -json | jq -r '.pass.value')
 
-echo ${HOST}${NAME}${USER}${PASS}
-#envsubst < blog.yaml > blog_tmp.yaml
-#kubectl apply -f blog_tmp.yaml
+envsubst < blog.yaml > blog_tmp.yaml
+kubectl apply -f blog_tmp.yaml
+kubectl apply -f service_blog.yaml
+kubectl apply -f ingress.yaml
