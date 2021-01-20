7c83ea5579ad956f140525002dd55a8ef3b61d84
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-20 13:03:32 +0100

    Private

--- blog/terraform/all/modules/sql/outputs.tf
+++ blog/terraform/all/modules/sql/outputs.tf
@@ -1,5 +1,5 @@
 output "host_addres" {
-  value = google_sql_database_instance.main_primary.public_ip_address
+  value = google_sql_database_instance.main_primary.private_ip_address
 }
 output "base_name" {
   value = google_sql_database_instance.main_primary.name
