eaecbc2a971b8d8c6f8c7a234c68059d67b20473
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-20 12:39:29 +0100

    Added secrets

--- blog/terraform/all/deploy.sh
+++ blog/terraform/all/deploy.sh
@@ -1,11 +1,10 @@
 export IMAGE="c97151a"
 export HOST=$(terraform output -json | jq -r '.host.value')
 export NAME=$(terraform output -json | jq -r '.name.value')
-export USER=$(terraform output -json | jq -r '.user.value')
-export PASS=$(terraform output -json | jq -r '.pass.value')
+#export USER=$(terraform output -json | jq -r '.user.value')
+#export PASS=$(terraform output -json | jq -r '.pass.value')
 
 envsubst < blog.yaml > blog_tmp.yaml
 kubectl apply -f blog_tmp.yaml
 kubectl apply -f service_blog.yaml
 kubectl apply -f ingress.yaml
-#
